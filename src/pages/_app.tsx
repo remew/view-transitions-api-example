1b13f8bf96c20806fcc4caa63cc1574d2be3304b
remew <foolishremew@gmail.com>
2023-03-16 06:09:50 +0900

    configure react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useRef } from 'react'
 import '~/styles/globals.css'
+import { QueryClient } from '@tanstack/query-core'
+import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { Deferred } from '~/libs/Deferred'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   const deferredRef = useRef<Deferred | null>()
   const { events } = useRouter()
 
@@ -23,5 +27,9 @@ export default function App({ Component, pageProps }: AppProps) {
     })
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
