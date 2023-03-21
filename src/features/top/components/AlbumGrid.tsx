de1509848e758bddc8a9e5a575992626839b4eb2
remew <foolishremew@gmail.com>
2023-03-22 00:52:25 +0900

    update top page

---
+++ src/features/top/components/AlbumGrid.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './AlbumGrid.module.css'
+import { useAlbumList } from '~/features/album/hooks/useAlbumList'
+
+export const AlbumGrid = () => {
+  const { data } = useAlbumList()
+
+  // suspenseを使用しているため本来不要だが、型チェックのためチェックを行う
+  if (!data) {
+    return null
+  }
+
+  return (
+    <ul className={styles.grid}>
+      {data.map(({ title, id, thumbnail, slug }) => {
+        return (
+          <li key={id}>
+            <Link href={`/${slug}`} className={styles.gridItem}>
+              <Image src={thumbnail} alt={title} width={128} height={128} className={styles.thumbnail} />
+              <h3 className={styles.albumName}>{title}</h3>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
